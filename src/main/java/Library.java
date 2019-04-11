6e08f0041b13faee7a3515fe5b1cb6250cb0cbd1
wintonlam <wintonlam@10.89.221.140>
2019-04-11 14:25:59 +0800

    /

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -9,6 +9,7 @@ public class Library {
 		System.out.println("When there is a fire, commit and push.");
 		System.out.println("When there is a fire, commit and push.");
 		System.out.println("When there is a fire, commit and push.");
+		int i;
 	}
     public boolean someLibraryMethod() {
         return true;
