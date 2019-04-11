cd6d2dbd5b6cc142d72b124458b8af847a92aa49
wintonlam <wintonlam@10.89.221.140>
2019-04-11 14:17:58 +0800

    cout

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -5,7 +5,7 @@ public class Library {
 	public static void main(String[] arg)
 	{
 		Library lib = new Library();
-		if(lib.someLibraryMethod())
+		//if(lib.someLibraryMethod())
 		System.out.println("When there is a fire, commit and push.");
 	}
     public boolean someLibraryMethod() {
