7a1e60a84e351716d73ae80913722a805ce1e5d4
cwwlam <cwwlam@csl4wk14.win2k.cs.ust.hk>
2019-04-11 14:22:36 +0800

    cout on kim

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -7,6 +7,7 @@ public class Library {
 		Library lib = new Library();
 		//if(lib.someLibraryMethod())
 		System.out.println("When there is a fire, commit and push.");
+		System.out.println("When there is a fire, commit and push.");
 	}
     public boolean someLibraryMethod() {
         return true;
